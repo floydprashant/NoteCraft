038c50d5f1333f3717fa4ac9d76d5e0f7bcc5e89
Prashant Rawal <rawal48prashant@gmail.com>
2024-03-17 12:02:34 +0545

    frontend changes

--- capstone/settings.py
+++ capstone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_vars['DJANGO_SECRET_KEY']
+SECRET_KEY = 'django-insecure-@=@qh*_jj(f0eehk%cdky85b7ful%@!*e!tpx=na!&f)w(p#s2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
